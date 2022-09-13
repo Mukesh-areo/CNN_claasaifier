734e9bed28826386f6cc57c48a1bfa4fc03625aa
Mukesh-areo <mukeshbabu447@.com>
2022-09-14 01:48:59 +0530

    added required things

--- src/deepClassifier/constants/__init__.py
+++ src/deepClassifier/constants/__init__.py
@@ -2,4 +2,4 @@ from importlib.resources import path
 from pathlib import Path
 
 CONFIG_FILE_PATH=Path("configs/config.yaml")
-CONFIG_FILE_PATH=Path("Params.yaml")
+PARAMS_FILE_PATH=Path("Params.yaml")
