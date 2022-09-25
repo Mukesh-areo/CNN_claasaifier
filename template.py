e054b4300161c09c4a8f29ea4d9f3851559e0211
Mukesh-areo <mukeshbabu447@.com>
2022-09-25 21:22:57 +0530

    score change

--- template.py
+++ template.py
@@ -5,9 +5,9 @@ from pathlib import Path
 import logging
 
 logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s: ')
-package_name="deepClassifier"
+package_name="deepClassifier"                                                    # "deepClassifier" is the name of the package 
 
-list_of_files=[
+list_of_files=[                                                                  # list of files that contain the DeepClassifier class
     ".github/workflows/.gitkeep",
     f"src/{package_name}/__init__.py",
     f"src/{package_name}/Component/__init__.py",
@@ -32,10 +32,10 @@ list_of_files=[
     "research/trials.ipynb"
 ]
 
-for filepath in list_of_files:
+for filepath in list_of_files:                                            
     filepath=Path(filepath)
     filedir, filename =os.path.split(filepath)
-    if filedir !="":
+    if filedir !="":                                # check if file exists if not create it
         os.makedirs(filedir,exist_ok=True)
         logging.info(f"Creating directory:{filedir} for file:{filename}")
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
